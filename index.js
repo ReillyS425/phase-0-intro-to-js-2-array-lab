981eac42587127390d3e8c3cdec019aaded290ed
Reilly Shofner <reilly.shofner@gmail.com>
2023-12-02 20:02:48 -0800

    complete js array lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+
+
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+function destructivelyAppendCat (name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat (name) {
+    cats.unshift(name)
+}
+
+// const destructivelyPrependCat = (name) => {
+//     cats.unshift(name)
+// }
+
+function destructivelyRemoveLastCat () {
+    cats.splice(-1)
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.splice(0, 1)
+}
+
+function appendCat (name) {
+ const newArray = [...cats, name]
+return newArray
+}
+
+function prependCat (name) {
+const newArray = [name, ...cats]
+return newArray;
+}
+
+function removeLastCat() {
+    const slicingLastCat = cats.slice(0, cats.length - 1)
+    return slicingLastCat
+}
+
+function removeFirstCat() {
+    const slicingFirstCat = cats.slice(1)
+return slicingFirstCat;
+}
